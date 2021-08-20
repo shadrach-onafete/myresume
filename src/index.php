4a71a3842456f0ad95c7442e5c84c35a46a138e5
shadrach-onafete <89081666+shadrach-onafete@users.noreply.github.com>
2021-08-20 18:52:18 +0100

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,235 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Onafete Shadrach Ufuoma</title>
+    <link href="https://fonts.googleapis.com/css?family=Oxygen:400,700|Arimo:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/80ef90cc53037e505eb827ce6/377d9655bc0ceacabc31010d7.js");</script>
+</head>
+<body class="is-boxed has-animations">
+    <div class="body-wrap boxed-container">
+        <header class="site-header">
+            <div class="container">
+                <div class="site-header-inner">
+                    <div class="brand header-brand">
+                        <h1 class="m-0">
+                            <a href="#">
+                                <img src="src/images/zuri-training-img.svg" alt="Blue logo">
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <section class="hero">
+				<div class="hero-left-decoration is-revealing"></div>
+				<div class="hero-right-decoration is-revealing"></div>
+                <div class="container">
+                    <div class="hero-inner">
+						<div class="hero-copy">
+	                        <h1 class="hero-title mt-0 is-revealing">Welcome to Onafete Shadrach's Portfolio</h1>
+	                        <p class="hero-paragraph is-revealing">I am a web developer with 3+ years of experince in designing and developing user interfaces, testing, debugging with eCommerce technologies. I am versatile and I like to play video games during leisure.</div><p>
+	                        <p class="hero-cta mb-0 is-revealing">
+							</p>
+						</div>
+						<div class="hero-illustration">
+						</div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="clients section">
+                <div class="container">
+                    <div class="clients-inner section-inner has-top-divider">
+                        <div class="container-sm">
+                            <ul class="list-reset mb-0">
+                                <li class="is-revealing">
+                                   <a href="https://www.facebook.com/onafete.shadrach"><img src="dist/images/facebook-logo.svg" alt="Facebook logo"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+			<section class="features section text-center">
+                <div class="container">
+                    <div class="features-inner section-inner has-top-divider">
+						<div class="features-header text-center">
+							<div class="container-sm">
+								<h2 class="section-title mt-0">Meet Shadrach</h2>
+								<p class="section-paragraph mb-0">I am a web developer with 3+ years of experince in designing and developing user interfaces, testing, debugging with eCommerce technologies. I am versatile and I like to play video games during leisure.</p>
+							</div>
+						</div>
+						<h2 class="section-title mt-0">SKILLS</h2>
+						<div class="features-wrap">
+                        	<div class="feature is-revealing">
+                                <div class="feature-inner">
+                                    <div class="feature-icon">
+                                        <img src="dist/images/feature-icon-02.svg" alt="Feature 02">
+                                    </div>
+                                    <h4 class="feature-title">WEB Development</h4>
+                                    <p class="text-sm">HTML, CSS, JAVASCRIPT, PHP, PYTHON</p>
+                                </div>
+                            </div>
+                            <div class="feature is-revealing">
+                                <div class="feature-inner">
+                                    <div class="feature-icon">
+                                        <img src="dist/images/feature-icon-01.svg" alt="Feature 01">
+                                    </div>
+                                    <h4 class="feature-title">HARDWARE ENGINEERING</h4>
+                                    <p class="text-sm"></p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+			<section class="features-tabs section">
+                <div class="container">
+                    <div class="features-tabs-inner section-inner has-top-divider">
+						<div class="tabs-container">
+							<ul class="tabs-links">
+								<li class="is-revealing">
+									<li class="is-revealing">
+									<a href="#tab-1" class="tab-link">
+										<img src="dist/images/tabs-icon-03.svg" alt="Tabs icon 03">
+										<span class="h4 m-0">B.Sc Computer Science</span>
+									</a>
+								</li>
+									<a href="#tab-2" class="tab-link is-active">
+										<img src="dist/images/tabs-icon-01.svg" alt="Tabs icon 01">
+										<span class="h4 m-0">Cybersecurity and the X-factor Certificate</span>
+									</a>
+								</li>
+								<li class="is-revealing">
+									<a href="#tab-3" class="tab-link">
+										<img src="dist/images/tabs-icon-02.svg" alt="Tabs icon 02">
+										<span class="h4 m-0">Ubuntu Member</span>
+									</a>
+								</li>
+								<li class="is-revealing">
+									<a href="#tab-4" class="tab-link">
+										<img src="dist/images/tabs-icon-03.svg" alt="Tabs icon 03">
+										<span class="h4 m-0">Web Development Technologies</span>
+									</a>
+								</li>
+								
+							</ul>
+							<div class="tabs-content">
+								<div id="tab-1" class="tab-panel">
+									<h2>B.Sc Computer Science</h2>
+									<p>2017/2021</p>
+									<p>Caleb University</p>
+									<p class="mb-0">I have a B.Sc in Computer Science from Caleb University.</p>
+								</div>
+								<div id="tab-2" class="tab-panel is-active">
+									<h2>Cybersecurity and the X-factor Certificate</h2>
+									
+										<p class="mb-0"></p><b>Kennesaw State University, USA</b></p>
+										<p><br>In Cybersecurity, the X-Factor related to unknown and unpredictable human behavior within and outside of your organization.
+										 “No one really knows why humans do what they do”, (David K. Reynolds), and because of this organizations can be unprepared for malicious, untrained, or even best intentioned behavior that can cause alarm and sometimes irreparable harm.</p>
+								</div>
+								<div id="tab-3" class="tab-panel">
+									<h2>Ubuntu Member</h2>
+									<p class="mb-0"><b>Ubuntu Leaders Academy South Africa</b></p>
+									<p class="mb-0">I was equipped with the essential leadership traits and skills to help sustain peaceful co-existence in the work space and in the outside community.</p>
+								</div>
+								<div id="tab-4" class="tab-panel">
+									<h2>Web Development Technologies</h2>
+									<p>I have 3+ years of experience in frontend and backend develoment .</p>
+									<p class="mb-0">I previously interned with Century Gateways technologies.</p>
+								</div>
+								
+							</div>
+						</div>
+                    </div>
+                </div>
+            </section>
+
+			<section class="cta section">
+				<div class="container">
+					<div class="cta-inner section-inner is-revealing">
+						<h3 class="section-title mt-0">Would you like to drop a message</h3>
+						<script type = "text/javascript">
+							<!--
+							   function Warn() {
+								  alert ("Your message has been sent!");
+								  document.write ("Your message has been sent!");
+							   }
+							//-->
+						 </script>     
+						<form action="welcome.php" method="post">
+							Name: 
+							<br><input type="text" name="name"><br>
+							E-mail: 
+							<br><input type="text" name="email"><br>
+							Your Message: 
+							<br><input type="text" width="300" height="500" name="textbox" id="textbox">
+							
+						<div class="cta-cta">
+							<a class="button button-primary button-shadow" href="mailto:shadrachufuoma234@gmail.com" onclick="Warn();">Mail me now</a>
+						</div>
+					</form>
+					</div>
+				</div>
+			
+			</section>
+        </main>
+
+        <footer class="site-footer">
+            <div class="container">
+                <div class="site-footer-inner">
+                    <div class="brand footer-brand">
+                        <a href="https://training.zuri.team/">
+                            <img src="src/images/hng.png" alt="HNG logo" width="250px" height="300px">
+                        </a>
+                    </div>
+                    <ul class="footer-social-links list-reset">
+                        <li>
+                            <a href="https://www.facebook.com/onafete.shadrach">
+                                <span class="screen-reader-text">Facebook</span>
+                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6.023 16L6 9H3V6h3V4c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V6H13l-1 3H9.28v7H6.023z" fill="#FFF"/>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:boluwatifeeri@gmail.com"></a>
+                                <span class="screen-reader-text">Google</span>
+                                <?xml version="1.0" encoding="utf-8"?>
+                                <svg viewBox="0 0 24 24" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                                  <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
+                                    <path fill="#4285F4" d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z"/>
+                                    <path fill="#34A853" d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z"/>
+                                    <path fill="#FBBC05" d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z"/>
+                                    <path fill="#EA4335" d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z"/>
+                                  </g>
+                                </svg>                            </a>
+                        </li>
+						
+                        <li>
+                            <a href="whatsapp://send?phone=+2348146424671">
+                                <span class="screen-reader-text">Whatsapp</span>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="45" width="45" viewBox="-93.2412 -156.2325 808.0904 937.395"><defs><linearGradient x1=".5" y1="0" x2=".5" y2="1" id="a"><stop stop-color="#20B038" offset="0%"/><stop stop-color="#60D66A" offset="100%"/></linearGradient><linearGradient x1=".5" y1="0" x2=".5" y2="1" id="b"><stop stop-color="#F9F9F9" offset="0%"/><stop stop-color="#FFF" offset="100%"/></linearGradient><linearGradient xlink:href="#a" id="f" x1="270.265" y1="1.184" x2="270.265" y2="541.56" gradientTransform="scale(.99775 1.00225)" gradientUnits="userSpaceOnUse"/><linearGradient xlink:href="#b" id="g" x1="279.952" y1=".811" x2="279.952" y2="560.571" gradientTransform="scale(.99777 1.00224)" gradientUnits="userSpaceOnUse"/><filter x="-.056" y="-.062" width="1.112" height="1.11" filterUnits="objectBoundingBox" id="c"><feGaussianBlur stdDeviation="2" in="SourceGraphic"/></filter><filter x="-.082" y="-.088" width="1.164" height="1.162" filterUnits="objectBoundingBox" id="d"><feOffset dy="-4" in="SourceAlpha" result="shadowOffsetOuter1"/><feGaussianBlur stdDeviation="12.5" in="shadowOffsetOuter1" result="shadowBlurOuter1"/><feComposite in="shadowBlurOuter1" in2="SourceAlpha" operator="out" result="shadowBlurOuter1"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.21 0" in="shadowBlurOuter1"/></filter><path d="M576.337 707.516c-.018-49.17 12.795-97.167 37.15-139.475L574 423.48l147.548 38.792c40.652-22.23 86.423-33.944 133.002-33.962h.12c153.395 0 278.265 125.166 278.33 278.98.025 74.548-28.9 144.642-81.446 197.373C999 957.393 929.12 986.447 854.67 986.48c-153.42 0-278.272-125.146-278.333-278.964z" id="e"/></defs><g fill="none" fill-rule="evenodd"><g transform="matrix(1 0 0 -1 -542.696 1013.504)" fill="#000" fill-rule="nonzero" filter="url(#c)"><use filter="url(#d)" xlink:href="#e" width="100%" height="100%"/><use fill-opacity=".2" xlink:href="#e" width="100%" height="100%"/></g><path transform="matrix(1 0 0 -1 41.304 577.504)" fill-rule="nonzero" fill="url(#f)" d="M2.325 274.421c-.014-47.29 12.342-93.466 35.839-134.166L.077 1.187l142.314 37.316C181.6 17.133 225.745 5.856 270.673 5.84h.12c147.95 0 268.386 120.396 268.447 268.372.03 71.707-27.87 139.132-78.559 189.858-50.68 50.726-118.084 78.676-189.898 78.708-147.968 0-268.398-120.386-268.458-268.358"/><path transform="matrix(1 0 0 -1 31.637 586.837)" fill-rule="nonzero" fill="url(#g)" d="M2.407 283.847c-.018-48.996 12.784-96.824 37.117-138.983L.072.814l147.419 38.654c40.616-22.15 86.346-33.824 132.885-33.841h.12c153.26 0 278.02 124.724 278.085 277.994.026 74.286-28.874 144.132-81.374 196.678-52.507 52.544-122.326 81.494-196.711 81.528-153.285 0-278.028-124.704-278.09-277.98zm87.789-131.724l-5.503 8.74C61.555 197.653 49.34 240.17 49.36 283.828c.049 127.399 103.73 231.044 231.224 231.044 61.74-.025 119.765-24.09 163.409-67.763 43.639-43.67 67.653-101.726 67.635-163.469-.054-127.403-103.739-231.063-231.131-231.063h-.09c-41.482.022-82.162 11.159-117.642 32.214l-8.444 5.004L66.84 66.86z"/><path d="M242.63 186.78c-5.205-11.57-10.684-11.803-15.636-12.006-4.05-.173-8.687-.162-13.316-.162-4.632 0-12.161 1.74-18.527 8.693-6.37 6.953-24.322 23.761-24.322 57.947 0 34.19 24.901 67.222 28.372 71.862 3.474 4.634 48.07 77.028 118.694 104.88 58.696 23.146 70.64 18.542 83.38 17.384 12.74-1.158 41.11-16.805 46.9-33.03 5.791-16.223 5.791-30.128 4.054-33.035-1.738-2.896-6.37-4.633-13.319-8.108-6.95-3.475-41.11-20.287-47.48-22.603-6.37-2.316-11.003-3.474-15.635 3.482-4.633 6.95-17.94 22.596-21.996 27.23-4.053 4.643-8.106 5.222-15.056 1.747-6.949-3.485-29.328-10.815-55.876-34.485-20.656-18.416-34.6-41.16-38.656-48.116-4.053-6.95-.433-10.714 3.052-14.178 3.12-3.113 6.95-8.11 10.424-12.168 3.467-4.057 4.626-6.953 6.942-11.586 2.316-4.64 1.158-8.698-.579-12.172-1.737-3.475-15.241-37.838-21.42-51.576" fill="#FFF"/></g></svg>                            </a>
+                        </li>
+						
+                    </ul>
+                    <div class="footer-copyright">&copy; 2021 shadrach, all rights reserved</div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="dist/js/main.min.js"></script>
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'php',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'shadrach/myresume',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shadrach/myresume' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'php',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
